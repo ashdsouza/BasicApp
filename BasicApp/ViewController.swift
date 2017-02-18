6f9d2b55cb4bf6a9e564327ae99a2fb447d80b64
ashley_f_dsouza <ashley.f.dsouza@oracle.com>
2017-02-17 16:28:47 -0800

    Initial Commit

---
+++ BasicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicApp
+//
+//  Created by Ashley F Dsouza on 2/17/17.
+//  Copyright © 2017 Ashley F Dsouza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
